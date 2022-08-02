46bedcaf35f49303ddff128f228cb3d87a9b3ffe
AntonioFaloppa <antoniofaloppa@hotmail.com>
2022-08-02 17:39:47 -0400

    update: excuse generator exercise added

--- main.js
+++ main.js
@@ -235,4 +235,23 @@ const rapid = (myString) => {
 };
 let str = "John";
 console.log(rapid(str));
-////
+////18 Excuse Generator
+let who = ['My dog', 'My grandma', 'My cat', 'A car'];
+let action = ['ate', 'pissed', 'crushed', 'broked'];
+let what = ['my homework', 'the keys', 'the car', 'my notes'];
+let time = ['before the class', 'during my lunch', 'riding in the car', 'while drinking coffee'];
+
+const sentenceOfEveryList = (array) => {
+    let position = Math.floor(Math.random()*array.length);
+    return position;
+}
+const sentenceGenerator = (who, action, what, time) => { 
+    let sentence = `${who[sentenceOfEveryList(who)]} ${action[sentenceOfEveryList(action)]} ${what[sentenceOfEveryList(what)]} ${time[sentenceOfEveryList(time)]}`;
+    let sentenceFormated = sentence.charAt(0).toUpperCase() +  sentence.slice(1);
+    return sentenceFormated;
+}
+window.onload = () => {
+    let excuse = document.querySelector("#excuse");
+    excuse.innerHTML = sentenceGenerator(who, action, what, time);
+}
+////19
