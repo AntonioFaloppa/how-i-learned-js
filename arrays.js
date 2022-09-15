dcd53d7b01c780712ef3172517d0d92192542234
AntonioFaloppa <antoniofaloppa@hotmail.com>
2022-09-15 15:00:32 -0400

    update

--- arrays.js
+++ arrays.js
@@ -24,8 +24,19 @@ function hacerAlmuerzo (ingredientes, metodoDeCocina) {
 };
 console.log(hacerAlmuerzo(comprarIngredientes(), mezclar));
 console.log(hacerAlmuerzo(comprarIngredientes(), freir));
-//2
-
-
-
-
+////2
+function throwDice () {
+    const result = Math.floor(Math.random()* 6 +1);
+    return result;
+};
+const player1 = [];
+const player2 = [];
+for (let _throw = 1; _throw < 6; _throw++) {
+    const result1 = throwDice();
+    player1.push(result1);
+    const result2 = throwDice();
+    player2.push(result2);
+};
+console.log(player1);
+console.log(player2);
+////3
